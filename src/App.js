425372487760e9a9ac64dc4a503f3f4519bb2bfd
Rabnoor Batra <rabnoor.batra@usask.ca>
2022-07-21 15:41:57 -0600

    implement panning and zooming for canvas and svg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Links from './Links';
+
+
 
 function App() {
+
+  let dataSet1 = [{type: "line", source: {x: 0, y:0}, target: {x:100, y:100}, color: "red"}, { type: "line", source: {x: 0, y:100}, target: {x:300, y:0}}, {type: "line", source: {x: 250, y:0}, target: {x:500, y:100}},{type: "polygon", source: {x: 150, x1: 180, y:0, y1: 0}, target: {x:400, x1: 440, y:100, y1: 100}}, {type: "polygon", source: {x:400, x1: 440,y:0, y1: 0}, target: {x: 90, x1: 160,  y:100, y1: 100}}];
+  let dataSet2 = [{type: "line", source: {x: 0, y:0}, target: {x:100, y:100}}, { type: "line", source: {x: 0, y:100}, target: {x:300, y:0}}, {type: "line", source: {x: 250, y:0}, target: {x:500, y:100}},{type: "line", source: {x: 150, y:0}, target: {x:400, y:100}}, {type: "line", source: {x:400, y:0}, target: {x: 90,  y:100}}];
+  let dataSet3 = [{type: "polygon", source: {x: 0,x1: 0,y1:0, y:0}, target: {x:100,x1: 200, y1: 100, y:100}}, { type: "polygon", source: {x: 0,x1:200, y1:0 , y:0}, target: {x:300, x1:900, y:0, y1:0}}, {type: "polygon", source: {x: 250,x1:300, y1:0, y:0}, target: {x:500,x1:750,y1:100, y:100}, color: "red"},{type: "polygon", source: {x: 150, x1: 180, y:0, y1: 0}, target: {x:400, x1: 440, y:100, y1: 100}}, {type: "polygon", source: {x:400, x1: 440,y:0, y1: 0}, target: {x: 90, x1: 160,  y:100, y1: 100}}]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      <p>SVG</p>
+
+      <Links arrayCoordinates={dataSet1} type="canvas"/>
+      <p>SVG</p>
+
+      <Links arrayCoordinates={dataSet3} type="canvas"/>
+      <p>Canvas</p>
+      <Links arrayCoordinates={dataSet2} type="canvas"/>
+   </div>
   );
 }
 
