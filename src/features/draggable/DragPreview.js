b2d1ad9133a68ce4f4db9d8968ee595355b13287
TaylanDufresne <taylan.dufresne@gmail.com>
2023-05-25 15:30:26 -0600

    fixing drag preivew

--- src/features/draggable/DragPreview.js
+++ src/features/draggable/DragPreview.js
@@ -42,8 +42,8 @@ export const DragPreview = memo(function DragPreview({ item, groupID, width, hei
   else if (component === "bitmap") {
 
     let suffix = isDark ? "_track_dark" : "_track"
-    let location = 'http://hci-sandbox.usask.ca/image-server'
-
+    let location = 'https://hci-sandbox.usask.ca/image-server/'
+ 
     let directoryName, fileName, designation, image
     let split_id = item.split("_")
     if (split_id[1].includes("at")) {
